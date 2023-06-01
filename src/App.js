3f6b44c758d191277742583bf747ff86f6e8feee
Manish <manishthakur231690@gmail.com>
2023-06-01 13:05:29 +0530

    React_First

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './header';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+const [name, setName] = useState(""); 
+const [email, setEmail] = useState(""); 
+const [course, setCourse] = useState(""); 
+const [data, setData] = useState([]);
+
+
+
+function addData(){
+  setData([...data, {name, email, course}]);
+  setName("");
+  setEmail("");
+  setCourse("");
+}
+
+const remove = (index) => {
+  let arr = data;
+  arr.splice(index, 1);
+  setData([...arr]);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+
+      <Header/>
+
+
+      <form action='#'>
+        <div>
+        <Stack direction="row" spacing={2} style={{marginTop:'20px'}}>
+              <TextField  value={name} onChange={(event) => setName(event.target.value)} id="outlined-basic" label="Name" variant="outlined" required/>
+              <TextField type={email} value={email} onChange={(event) => setEmail(event.target.value)} id="outlined-basic" label="Email" variant="outlined" required />
+              <TextField value={course} onChange={(event) => setCourse(event.target.value)} id="outlined-basic" label="Course" variant="outlined" required/>
+              <Button type='submit' onClick={addData} variant="contained">Add</Button> 
+        </Stack>
+        </div>
+      </form>
+
+      <div className="data-container">
+        <div className="data-value">
+          <h3>Name</h3>
+          <h3>Email</h3>
+          <h3>Course</h3>
+          <h3>Remove</h3>
+
+        </div>
+
+        { data.map((element, index) => {
+            return (
+                          <div key={index} className="data-value">
+                          <p>{element.name}</p>
+                          <p>{element.email}</p>
+                          <p>{element.course}</p>
+                          <Button onClick={() => remove(index)} variant="contained" size="small" color="error"> Remove </Button>
+                          </div>
+                  
+                );
+          })
+        }
+      </div>
+    </>
   );
 }
 
---
+++ src/header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+
+
+const Header = () => {
+  return (
+    <div>
+            <header>
+                        <h1>Keep Records</h1>
+            </header>
+    </div>
+  )
+}
+
+export default Header
